ac9e2e350c87d5cf25184d388a303a703b23425e
Luis Noriega <91396191+noriegaencinas@users.noreply.github.com>
2024-03-14 20:18:28 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tarea de gestion de calidad
---
+++ script.js
@@ -0,0 +1,126 @@
+const targets = document.querySelectorAll('[data-target]')
+const content = document.querySelectorAll('[data-content]')
+
+targets.forEach(target => {
+    target.addEventListener('click', () => {
+        content.forEach(c => {
+            c.classList.remove('active')
+        })
+        const t = document.querySelector(target.dataset.target)
+        t.classList.add('active')
+    })
+})
+
+
+function holaMundo() {
+    console.log("hola mundo")
+}
+
+
+function generarOpciones_anti() {
+    const select = document.getElementById('opciones_anti');
+    if (select) { // Verifica si el elemento existe
+        for (let i = 1; i <= 100; i++) {
+            const opcion = document.createElement('option');
+            opcion.value = i;
+            opcion.textContent = `${i} Años`;
+            select.appendChild(opcion);
+        }
+    }
+}
+
+function generarOpciones_grado() {
+    const select = document.getElementById('opciones_grado');
+    if (select) { // Verifica si el elemento existe
+        const grados = ["Soldado", "Cabo", "Sargento Segundo", "Sargento Primero", "Subteniente", "Teniente", "Capitán Segundo", "Capitan Primero", "Mayor", "Teniente Coronel", "Coronel", "General de Brigadier", "General de Brigada", "General de División", "General de Secretario de la Defensa Nacional"]
+        for (let i = 1; i < grados.length; i++) {
+            const opcion = document.createElement('option');
+            opcion.value = i;
+            opcion.textContent = grados[i];
+            select.appendChild(opcion);
+        }
+    }
+}
+
+function validarNumero(id) {
+    const input = document.getElementById(id);
+    if (input.value < 0) {
+        input.value = ''; // Borra el valor si es negativo
+    }
+}
+
+function clickBuscar() {
+    const content = document.querySelectorAll('[data-content]')
+    content.forEach(c => {
+        c.classList.remove('active')
+    })
+    const t = document.getElementById("resultado")
+    t.classList.add('active')
+    const te = document.getElementById("texto_resultado")
+    te.textContent = "algo"
+}
+
+window.onload = function () {
+    generarOpciones_anti();
+    generarOpciones_grado();
+};
+
+function calculateSalary() {
+    validarNumero('baseSalary');
+    validarNumero('hoursWorked');
+    validarNumero('extraHours');
+
+    var salarioBase = parseFloat(document.getElementById('baseSalary').value);
+    var horasTrabajadas = parseFloat(document.getElementById('hoursWorked').value);
+    var horasExtras = parseFloat(document.getElementById('extraHours').value);
+
+    // Las horas extras se pagan al doble
+    var salarioHoraExtra = salarioBase * 2;
+    var salarioTotal = (horasTrabajadas * salarioBase) + (horasExtras * salarioHoraExtra);
+
+    var impuestos = calcularImpuestos(salarioTotal);
+    var salarioDiario = salarioTotal - impuestos;
+    var salarioSemanal = salarioDiario * 7;
+    var salarioQuincenal = salarioDiario * 15;
+    var salarioMensual = salarioDiario * 30;
+
+    document.getElementById('impuestos').textContent = "El impuesto total diario: " + impuestos.toFixed(2);
+    document.getElementById('salarioDiario').textContent = "El salario diario es: " + salarioDiario.toFixed(2);
+    document.getElementById('salarioSemanal').textContent = "El salario semanal es: " + salarioSemanal.toFixed(2);
+    document.getElementById('salarioQuincenal').textContent = "El salario quincenal es: " + salarioQuincenal.toFixed(2);
+    document.getElementById('salarioMensual').textContent = "El salario mensual es: " + salarioMensual.toFixed(2);
+}
+
+function calcularImpuestos(totalIngreso) {
+    // Definir las tasas de impuestos
+    var tasasImpuestos = [
+        { limiteInferior: 0.01, limiteSuperior: 24.54, cuotaFija: 0.00, porcentaje: 1.92 },
+        { limiteInferior: 24.54, limiteSuperior: 208.29, cuotaFija: 0.47, porcentaje: 6.40 },
+        { limiteInferior: 208.30, limiteSuperior: 366.05, cuotaFija: 12.23, porcentaje: 10.88 },
+        { limiteInferior: 366.06, limiteSuperior: 425.52, cuotaFija: 29.40, porcentaje: 16.00 },
+        { limiteInferior: 425.53, limiteSuperior: 509.46, cuotaFija: 38.91, porcentaje: 17.92 },
+        { limiteInferior: 509.47, limiteSuperior: 1024.52, cuotaFija: 53.95, porcentaje: 21.36 },
+        { limiteInferior: 1027.53, limiteSuperior: 1619.51, cuotaFija: 164.61, porcentaje: 23.52 },
+        { limiteInferior: 1619.52, limiteSuperior: 3091.90, cuotaFija: 303.85, porcentaje: 30.00 },
+        { limiteInferior: 3091.91, limiteSuperior: 4122.54, cuotaFija: 745.56, porcentaje: 32.00 },
+        { limiteInferior: 4122.55, limiteSuperior: 12367.62, cuotaFija: 1075.37, porcentaje: 34.00 },
+        { limiteInferior: 12367.63, cuotaFija: 3878.69, porcentaje: 35.00 }
+    ];
+
+    var impuestoTotal = 0;
+
+    // Calcular el impuesto para el rango de ingreso correcto
+    for (var i = 0; i < tasasImpuestos.length; i++) {
+        var rango = tasasImpuestos[i];
+        if (totalIngreso > rango.limiteInferior && (!rango.limiteSuperior || totalIngreso <= rango.limiteSuperior)) {
+            var ingresoImponible = totalIngreso - rango.limiteInferior;
+            impuestoTotal = rango.cuotaFija + (ingresoImponible * (rango.porcentaje / 100));
+            break;
+        }
+    }
+
+    return impuestoTotal;
+}
+
+function createAccount() {
+}
